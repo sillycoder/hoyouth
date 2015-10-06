616e64a4dea9abcb5d4275235c70638552432c6c
张华民 <zhuamin1023@163.com>
2015-10-06 16:57:07 +0800

    Update error.php

--- protected/views/index/error.php
+++ protected/views/index/error.php
@@ -1,1 +1,10 @@
-<img src="images/error.jpg" />
+<!DOCTYPE html>
+<html>
+    <head lang="en">
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+<img src="images/error.jpg" width="100%" />
+</body>
+    </html>
