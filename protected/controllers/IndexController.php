7b847c28e6c0411d578f33910fdcd19d9571b2ad
张华民 <zhuamin1023@163.com>
2015-10-06 16:49:40 +0800

    Update IndexController.php

--- protected/controllers/IndexController.php
+++ protected/controllers/IndexController.php
@@ -7,7 +7,7 @@ class IndexController extends Controller
         // renders the view file 'protected/views/site/index.php'
         // using the default layout 'protected/views/layouts/main.php'
         //$this->render('index');
-        $this->render('error');
+        $this->renderpartial('error');
     }
 
 }
