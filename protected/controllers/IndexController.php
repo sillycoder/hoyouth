82ba3bf733b9f391cbcd99f1a8a3b5c664c6ef2b
张华民 <zhuamin1023@163.com>
2015-10-07 18:49:58 +0800

    Update IndexController.php

--- protected/controllers/IndexController.php
+++ protected/controllers/IndexController.php
@@ -6,6 +6,7 @@ class IndexController extends Controller
     {
         // renders the view file 'protected/views/site/index.php'
         // using the default layout 'protected/views/layouts/main.php'
+        header("Location:http://hoyouth.com/shop");
         $this->render('index');
         //$this->renderpartial('error');
     }
