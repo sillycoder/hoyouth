9b4c2279bc08c440e2ddeb83e5cb453cdca54c4a
liujianwei <jwviva@sina.com>
2015-10-08 12:37:17 +0800

    update shop detail

--- protected/views/shop/index.php
+++ protected/views/shop/index.php
@@ -4,22 +4,23 @@
     $(document).ready(function() {
         $('#fullpage').fullpage({
             verticalCentered: false,
-            'sectionsColor': ['#fff','#f9f9f9','#707070','#f9f9f9','#fff','#fff','transparent','#fff','#fff']
+            'sectionsColor': ['#efefef','#fff','#fff','#fff','#f9f9f9','#fff','#f9f9f9','#fff','#d5d0d6','#89898b']
         });
     });
 </script>
 <style>
     #header, .shop-sub-header{position: fixed;z-index: 10;}
     .shop-sub-header{top: 40px;}
-    #section0{background: url(http://attach.bbs.miui.com/album/201509/25/163735la38z766dphr1zaa.png) center bottom no-repeat transparent;}
-    #section1{background: url(http://attach.bbs.miui.com/album/201509/25/163735wy09o190sk3rbjgo.png) center 0 no-repeat transparent;}
-    #section2{background: url(http://attach.bbs.miui.com/album/201509/25/163736xxydonakdo3dw3hn.jpg) center 0 no-repeat transparent;}
-    #section3{background: url(http://attach.bbs.miui.com/album/201509/25/163736m38mlznra98zuqz4.png) center 0 no-repeat transparent;}
-    #section4{background: url(http://attach.bbs.miui.com/album/201509/25/163736rh68qkkh551b6hoo.png) center 0 no-repeat transparent;}
-    #section5{background: url(http://attach.bbs.miui.com/album/201509/25/163737espschseos8wch8h.jpg) center 0 no-repeat transparent;}
-    #section6{background: url(http://attach.bbs.miui.com/album/201509/25/163737w737qnww08nkun7n.png) center 0 no-repeat transparent;}
-    #section7{background: url(http://attach.bbs.miui.com/album/201509/25/163737ytck3ko67zze8yoo.png) center 0 no-repeat transparent;}
-    #section8{background: url(http://attach.bbs.miui.com/album/201509/25/163738os9g9p99rqcw9j14.png) center 0 no-repeat transparent;}
+    #section0{background: url(http://attach.bbs.miui.com/album/201510/08/121603zv1d70692ge971ff.jpg) center bottom no-repeat transparent;}
+    #section1{background: url(http://attach.bbs.miui.com/album/201510/08/121603xqn899fl11fdq79w.jpg) center 0 no-repeat transparent;}
+    #section2{background: url(http://attach.bbs.miui.com/album/201510/08/121604bny3i3ym990qyf20.jpg) center 0 no-repeat transparent;}
+    #section3{background: url(http://attach.bbs.miui.com/album/201510/08/121604u1a61z119l63psl6.jpg) center 0 no-repeat transparent;}
+    #section4{background: url(http://attach.bbs.miui.com/album/201510/08/121604hvuzrhp4cz7d7m22.jpg) center 0 no-repeat transparent;}
+    #section5{background: url(http://attach.bbs.miui.com/album/201510/08/121604cz8r8ri8yvvgo7u5.jpg) center 0 no-repeat transparent;}
+    #section6{background: url(http://attach.bbs.miui.com/album/201510/08/121604ttb0gbzcyynbgebc.jpg) center 0 no-repeat transparent;}
+    #section7{background: url(http://attach.bbs.miui.com/album/201510/08/121605gr8b6rpdzzpjur98.jpg) center 0 no-repeat transparent;}
+    #section8{background: url(http://attach.bbs.miui.com/album/201510/08/121605x6u7uoo6t2qucrco.jpg) center 0 no-repeat transparent;}
+    #section9{background: url(http://attach.bbs.miui.com/album/201510/08/123520z4fsgqbzxwwshmaa.jpg) center 0 no-repeat transparent;}
 </style>
 <div id="fullpage">
     <div class="section " id="section0"></div>
@@ -34,4 +35,5 @@
     <div class="section" id="section6"></div>
     <div class="section" id="section7"></div>
     <div class="section" id="section8"></div>
+    <div class="section" id="section9"></div>
 </div>
