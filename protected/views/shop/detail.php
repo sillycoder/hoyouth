1ef5b636f70364e01b6dbcc60e5a1abb74dd181b
liujianwei <jwviva@sina.com>
2015-10-08 12:49:55 +0800

    update detail page

--- protected/views/shop/detail.php
+++ protected/views/shop/detail.php
@@ -1,30 +1,30 @@
-<script type="text/javascript" src="/js/jquery.1.7.1.min.js"></script>
-<script type="text/javascript" src="/js/jquery.fullPage.min.js"></script>
+<!--<script type="text/javascript" src="/js/jquery.1.7.1.min.js"></script>-->
+<!--<script type="text/javascript" src="/js/jquery.fullPage.min.js"></script>-->
 
 <script type="text/javascript">
-    $(document).ready(function() {
-        $('#fullpage').fullpage({
-            verticalCentered: false,
-            'sectionsColor': ['#fff','#f9f9f9','#707070','#f9f9f9','#fff','#fff','transparent','#fff','#fff']
-        });
-    });
+//    $(document).ready(function() {
+//        $('#fullpage').fullpage({
+//            verticalCentered: false,
+//            'sectionsColor': ['#fff','#f9f9f9','#707070','#f9f9f9','#fff','#fff','transparent','#fff','#fff']
+//        });
+//    });
 </script>
 
 <style>
-    #header, .shop-sub-header{position: fixed;z-index: 10;}
-    .shop-sub-header{top: 40px;}
-    #section0{background: url(http://hoyouth.com/images/bf_detail_02.jpg) center bottom no-repeat transparent;}
-    #section1{background: url(http://hoyouth.com/images/bf_detail_03.jpg) center 0 no-repeat transparent;}
-    #section2{background: url(http://hoyouth.com/images/bf_detail_04.jpg) center 0 no-repeat transparent;}
-    #section3{background: url(http://hoyouth.com/images/bf_detail_05.jpg) center 0 no-repeat transparent;}
-    #section4{background: url(http://hoyouth.com/images/bf_detail_06.jpg) center 0 no-repeat transparent;}
-    #section5{background: url(http://hoyouth.com/images/bf_detail_07.jpg) center 0 no-repeat transparent;}
-    #section6{background: url(http://hoyouth.com/images/bf_detail_08.jpg) center 0 no-repeat transparent;}
-    #section7{background: url(http://hoyouth.com/images/bf_detail_09.jpg) center 0 no-repeat transparent;}
-    #section8{background: url(http://hoyouth.com/images/bf_detail_10.jpg) center 0 no-repeat transparent;}
-    #section9{background: url(http://hoyouth.com/images/bf_detail_11.jpg) center 0 no-repeat transparent;}
-    #section10{background: url(http://hoyouth.com/images/bf_detail_12.jpg) center 0 no-repeat transparent;}
-    #section11{background: url(http://hoyouth.com/images/bf_detail_13.jpg) center 0 no-repeat transparent;}
+    /*#header, .shop-sub-header{position: fixed;z-index: 10;}*/
+    /*.shop-sub-header{top: 40px;}*/
+    #section0{background: url(http://hoyouth.com/images/bf_detail_02.jpg) center bottom no-repeat transparent;height: 749px;}
+    #section1{background: url(http://hoyouth.com/images/bf_detail_03.jpg) center 0 no-repeat transparent;height: 923px;}
+    #section2{background: url(http://hoyouth.com/images/bf_detail_04.jpg) center 0 no-repeat transparent;height: 750px;}
+    #section3{background: url(http://hoyouth.com/images/bf_detail_05.jpg) center 0 no-repeat transparent;height: 764px;}
+    #section4{background: url(http://hoyouth.com/images/bf_detail_06.jpg) center 0 no-repeat transparent;height: 751px;}
+    #section5{background: url(http://hoyouth.com/images/bf_detail_07.jpg) center 0 no-repeat transparent;height: 500px;}
+    #section6{background: url(http://hoyouth.com/images/bf_detail_08.jpg) center 0 no-repeat transparent;height: 500px;}
+    #section7{background: url(http://hoyouth.com/images/bf_detail_09.jpg) center 0 no-repeat transparent;height: 505px;}
+    #section8{background: url(http://hoyouth.com/images/bf_detail_10.jpg) center 0 no-repeat transparent;height: 750px;}
+    #section9{background: url(http://hoyouth.com/images/bf_detail_11.jpg) center 0 no-repeat transparent;height: 749px;}
+    #section10{background: url(http://hoyouth.com/images/bf_detail_12.jpg) center 0 no-repeat transparent;height: 746px;}
+    #section11{background: url(http://hoyouth.com/images/bf_detail_13.jpg) center 0 no-repeat transparent;height: 749px;}
 </style>
 <div id="fullpage">
     <div class="section " id="section0"></div>
